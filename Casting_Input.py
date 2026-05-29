53a8021ce3f5407d6200ada15db5cd706839f7d7
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-29 16:04:45 +0530

    Type Casting

---
+++ Casting_Input.py
@@ -0,0 +1,17 @@
+#Type Casting : The Data Change one Data type to another data type
+# example : str(), int(), Float(), Bool()
+
+Age = 25
+age = str(Age)
+print(type(age))
+
+Number = float(Age)
+print(Number)
+
+name = ""
+name = bool(name)
+print(name)
+
+Name = "vamsi"
+name = bool(Name)
+print(name)
