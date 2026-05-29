33dadb5e254a4888133cfd7505f0c054f7e88523
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-29 17:09:13 +0530

    Sqft Calculator

---
+++ Sqft-Calc.py
@@ -0,0 +1,7 @@
+length = float(input('Enter the Height or Length in Meter:'))
+width = float(input('Enter the Breath or Width in meter:'))
+Area = length * width
+print(f"the Area is {Area}")
+Sqft = (f"Total {Area * 10.7639} sq ft")
+print(Sqft)
+
