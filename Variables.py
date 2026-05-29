ed7a5ccf8805ab3a9b4a7cb6cb58db13fc8590c1
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-29 14:56:30 +0530

    variables

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#intro
+print (" Python_programme :")
+
+#  Variable : 4 types 
+# 1. integer
+# 2. String 
+# 3. Float
+# 4. boolean 
+
+Full_Name = "Vamsi Mamidi"
+Age = 22
+CGP = 81.7
+Student = True
+
+print(f"Your Name is {Full_Name}, Age is {Age} old. and your Marks id {CGP} CGP ")
