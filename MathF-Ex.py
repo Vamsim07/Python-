1d421b1aeeb85f10b5cfda0912a0134848587889
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-31 00:18:35 +0530

    Radius

--- MathF-Ex.py
+++ MathF-Ex.py
@@ -1,9 +1,17 @@
-# 2(pi)r = Circumference
-
 import math
 
+# 1) 2(pi)r = Circumference
+
+"""
 radius = float(input("Enter the Radius : "))
 Circumference = 2 * math.pi * radius
 
 print(f"The Circumference is {round(Circumference, 2)} cm")
 
+"""
+# 2) Area = pi * r^2
+
+Radius = float(input("Enter the Radius : "))
+Area = math.pi * Radius**2
+print(f"The Radius is {round(Area, 2)} cm")
+print(math.pi)
