2fe540c20cb2ca9cd2031a6df0432028051437f1
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-30 23:55:28 +0530

    Circumference

---
+++ MathF-Ex.py
@@ -0,0 +1,9 @@
+# 2(pi)r = Circumference
+
+import math
+
+radius = float(input("Enter the Radius : "))
+Circumference = 2 * math.pi * radius
+
+print(f"The Circumference is {round(Circumference, 2)} cm")
+
