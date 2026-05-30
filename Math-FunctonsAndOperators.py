0f4e38783f6119cf0781847a5910ec4fbf76fe41
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-30 17:17:37 +0530

    Operators

---
+++ Math-FunctonsAndOperators.py
@@ -0,0 +1,44 @@
+# Operators
+#Edision
+A = 20 
+B = 25
+print(A+B)
+C = A + B
+print(C)
+A += B
+print(A)
+
+# SubStraction
+A = 20 
+B = 25
+print(A - B)
+C = A - B
+A -= B
+print(C)
+print(A)
+
+# Multiply
+A = 20 
+B = 25
+print(A * B)
+A *= B
+print(A)
+
+#Devision
+A = 20 
+B = 5
+print(A / B)
+A /= B
+print(A)
+
+
+A = 20 
+B = 6
+print(A % B)
+A %= B
+print(A)
+
+#powers
+A = 3
+A **= 2
+print(A)
