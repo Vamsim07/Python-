c2716e25aa7b911b7780d5c642f23ebd322397f0
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-30 17:31:56 +0530

    Math Functions

--- Math-FunctonsAndOperators.py
+++ Math-FunctonsAndOperators.py
@@ -1,44 +1,58 @@
-# Operators
-#Edision
-A = 20 
-B = 25
-print(A+B)
-C = A + B
-print(C)
-A += B
-print(A)
+# # Operators
+# #Edision
+# A = 20 
+# B = 25
+# print(A+B)
+# C = A + B
+# print(C)
+# A += B
+# print(A)
 
-# SubStraction
-A = 20 
-B = 25
-print(A - B)
-C = A - B
-A -= B
-print(C)
-print(A)
+# # SubStraction
+# A = 20 
+# B = 25
+# print(A - B)
+# C = A - B
+# A -= B
+# print(C)
+# print(A)
 
-# Multiply
-A = 20 
-B = 25
-print(A * B)
-A *= B
-print(A)
+# # Multiply
+# A = 20 
+# B = 25
+# print(A * B)
+# A *= B
+# print(A)
 
-#Devision
-A = 20 
-B = 5
-print(A / B)
-A /= B
-print(A)
+# #Devision
+# A = 20 
+# B = 5
+# print(A / B)
+# A /= B
+# print(A)
 
 
-A = 20 
-B = 6
-print(A % B)
-A %= B
-print(A)
+# A = 20 
+# B = 6
+# print(A % B)
+# A %= B
+# print(A)
 
-#powers
-A = 3
-A **= 2
-print(A)
+# #powers
+# A = 3
+# A **= 2
+# print(A)
+
+
+
+#Math Functions
+import math
+
+a = 10.764
+print(round(a)) #it's convert boolean Values into integer
+B = -10.76
+print(abs(B))  #It's Convert negative values into Positive
+C = 5
+print(pow(C, 2))  # Power of C^2
+print(max(a, B, C)) # Maximum Value
+print(min(a, B, C)) #Minimum Value
