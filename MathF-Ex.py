47d1992e0852c7dac2d13a5d56fae958dc69c08c
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-31 00:30:07 +0530

    Hypotenuese

--- MathF-Ex.py
+++ MathF-Ex.py
@@ -10,8 +10,18 @@ print(f"The Circumference is {round(Circumference, 2)} cm")
 
 """
 # 2) Area = pi * r^2
-
+"""
 Radius = float(input("Enter the Radius : "))
 Area = math.pi * Radius**2
 print(f"The Radius is {round(Area, 2)} cm")
 print(math.pi)
+
+"""
+# 3) RootOver(A^2 +B^2) Hypotenuese
+
+A = int(input("Enter a Number : "))
+B = int(input("Enter Another Number : "))
+
+C  = math.sqrt(A**2 + B**2)
+
+print(f"Hypotenuese is {C} ")
