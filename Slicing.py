48fe96e1fceb0c4e25d6daddbfc48fe5c6e5df2a
Vamsim07 <vamsimamidi197@gmail.com>
2026-05-31 23:55:57 +0530

    indexing

---
+++ Slicing.py
@@ -0,0 +1,18 @@
+"""
+
+Slicing: it 2 different ways 
+1) indexing
+2) Slice() method
+
+
+"""
+Name = "Vamsi Mamidi"
+First_Name =Name[:5] #if you use [0:6] like this
+Last_Name = Name[6 : ] # use like [6 : 12]
+
+ReversName = Name[::-1] #[0:12:-1] -1 for reverse
+
+
+print(First_Name)
+print(Last_Name)
+print(ReversName)
