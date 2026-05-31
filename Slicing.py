1f31ea53d0eb1b7ec32b8ab5f1e8eba95d542dc0
Vamsim07 <vamsimamidi197@gmail.com>
2026-06-01 00:13:23 +0530

    slice() Method

--- Slicing.py
+++ Slicing.py
@@ -16,3 +16,18 @@ ReversName = Name[::-1] #[0:12:-1] -1 for reverse
 print(First_Name)
 print(Last_Name)
 print(ReversName)
+
+#Slice()
+
+Website = "https://google.com"
+Slice = slice(8,-4)
+
+print(Website[Slice])
+
+My_Name = "Vamsi Mamidi"
+
+s = slice(0,-7)
+i = slice(6,None) # None meaning until end
+
+print(My_Name[s])
+print(f"my initial is : {My_Name[i]}")
