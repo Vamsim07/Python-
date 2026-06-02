28b9d2cfe5ec0a7305ccab09e3742c9b1c6e909e
Vamsim07 <vamsimamidi197@gmail.com>
2026-06-02 10:35:06 +0530

    Driving Contest Age

---
+++ Driving_Contest_Using_If.py
@@ -0,0 +1,18 @@
+# Statement : If ,elif, else
+
+print("Driving Contest")
+
+Age = int(input("Enter Your Age : "))
+
+if Age >= 18 and Age < 80:
+    print("you are eligible! Enjoy your ride..!")
+
+elif Age >= 80:
+    print("Your Age limit exceeded")
+
+else:
+    print("Sorry, You are not eligible for this contest.")
+    
+
+
+
