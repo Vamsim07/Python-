625663d59152510ab2c13be22335f055cbcf95a1
Vamsim07 <vamsimamidi197@gmail.com>
2026-06-03 22:05:35 +0530

    NOT & OR

--- LogicalOperator.py
+++ LogicalOperator.py
@@ -2,12 +2,21 @@
 
 temp = int(input("enter the Temprator at outside : "))
 
-if -100 <= temp <= 10 : 
-    print("the Temperator is cool, and Njoy the snow")
+# if -100 <= temp <= 10 : 
+#     print("the Temperator is cool, and Njoy the snow")
 
-elif temp >= 10 and temp <= 30:    # Using AND Operator
-    print("The Temperator is Great")
+# elif not(temp >= 10 and temp <= 30):
+#     print("mistaken")
+
+# elif temp >= 10 and temp <= 30:    # Using AND Operator
+#    print("The Temperator is Great")
+    
+
+# else:
+#     print("The Temprator is high..! Be Hydrate")
+
+if (temp >= 10 and temp <= 30) or (-100 <= temp <= 10) :
+    print("good whether")
 
 else:
-    print("The Temprator is high..! Be Hydrate")
-
+    print("Avoiding outside..!")
