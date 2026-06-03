a2864347bc1eccb185d49827017342137afc854a
Vamsim07 <vamsimamidi197@gmail.com>
2026-06-03 22:48:27 +0530

    for loop

---
+++ ForLoop.py
@@ -0,0 +1,14 @@
+# #1
+# for i in range(10,-1,-1):
+#     print(i)
+# #2  
+# for J in "vamsi":
+#     print(J)
+
+#3
+
+import time
+for sec in range(10,-1,-1):
+    print(sec)
+    time.sleep(1)
+print("Happy New Year")
