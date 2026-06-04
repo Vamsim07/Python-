6dcf646594c48190bb573857d7d79f2bca85e045
Vamsim07 <vamsimamidi197@gmail.com>
2026-06-04 08:40:56 +0530

    Break-Continue-Pass

---
+++ LoopControle.py
@@ -0,0 +1,21 @@
+# break
+# continue
+# pass
+
+# while True:
+#     Name = input("Enter Your Name :")
+#     if Name != "":
+#         break   # using Break Statement
+# print("Hello "+Name)
+
+# PhoneNumber = "9948-6666-66"
+# for i in PhoneNumber:
+#     if i == "-":
+#         continue  # Using Continue Statement
+#     print(i,end="")
+
+for i in range(1,21):
+    if i == 13:
+        pass
+    else:
+        print(i, end=" ")
